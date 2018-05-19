9c02becd50239a2ceee73889406d275ae7d5145b
AteneaPallares <35583225+AteneaPallares@users.noreply.github.com>
2018-05-18 21:16:05 -0500

    Credito
    Iniciales de la tarjetas de credito
    Dinersclub
    300-305, 309, 36, 38-39
    Jcb
    3528-3589
    Discover
    6011, 622126-622925, 644-649, 65

--- Models/Tarjeta.cs
+++ Models/Tarjeta.cs
@@ -76,6 +76,11 @@ namespace CreditoWeb.Models
             if(Valida==true){
                 int m=(int)Char.GetNumericValue(TarjetaNum[0]);
                 int r=(int)Char.GetNumericValue(TarjetaNum[1]);
+                int o=(int)Char.GetNumericValue(TarjetaNum[2]);
+                int p=(int)Char.GetNumericValue(TarjetaNum[3]);
+                int t=(int)Char.GetNumericValue(TarjetaNum[4]);
+                int u=(int)Char.GetNumericValue(TarjetaNum[5]);
+
                 if(((m==3 && r==4)||(m==3 && r==7))&&TarjetaNum.Length==15){
                     soluci=TipoTarjeta.AMERICANEXPRESS;
                 }
@@ -85,6 +90,35 @@ namespace CreditoWeb.Models
                 if((m==4)&&(TarjetaNum.Length==16 || TarjetaNum.Length==13)){
                     soluci=TipoTarjeta.VISA;
                 }
+                //Dinersclub
+                //300-305, 309, 36, 38-39
+                if((m==3 && r==0 && o==0)||(m==3 && r==0 && o==1)||(m==3 && r==0 && o==2)||(m==3 && r==0 && o==3)||(m==3 && r==0 && o==4)||(m==3 && r==0 && o==5)||(m==3 && r==0 && o==9)||(m==3 && r==6)||(m==3 && r==8)||(m==3 && r==9)||(m==3 && r==0 && o==7)){
+                     soluci=TipoTarjeta.DINERSCLUB;
+                }
+                 //Jcb
+                //3528-3589
+                for(int i=28;i<=89;i++){
+                    string ok=i.ToString();
+                    int y=(int)Char.GetNumericValue(ok[0]);
+                    int q=(int)Char.GetNumericValue(ok[1]);
+                if((m==3 && r==5 && o==y && p==q)){
+                     soluci=TipoTarjeta.JCB;
+                     break;
+                }
+                }
+                //Discover
+                //​6011, 622126-622925, 644-649, 65
+                 for(int i=126;i<=925;i++){
+                    string ok=i.ToString();
+                    int y=(int)Char.GetNumericValue(ok[0]);
+                    int q=(int)Char.GetNumericValue(ok[1]);
+                    int z=(int)Char.GetNumericValue(ok[2]);
+                if((m==6 && r==2 && o==2 && p==y && t==q && u==z)||(m==6 && r==0 && o==1 && p==1)||(m==6 && r==4 && o==4)||(m==6 && r==4 && o==5)||(m==6 && r==4 && o==6)||(m==6 && r==4 && o==7)||(m==6 && r==4 && o==8)||(m==6 && r==4 && o==9)||(m==6 && r==5)){
+                     soluci=TipoTarjeta.DISCOVER;
+                     break;
+                }
+                }
+
             }
             return soluci;
         }
@@ -98,6 +132,9 @@ namespace CreditoWeb.Models
         VISA,
         MASTERCARD,
         AMERICANEXPRESS,
+        DINERSCLUB,
+        JCB,
+        DISCOVER,
         NOVALIDA
 
 
